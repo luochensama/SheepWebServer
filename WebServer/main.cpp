cd2946e68c49bd48982a943264978308eade205a
luochensama <350119763@qq.com>
2022-03-15 16:38:11 +0800

    Fixed Bug

--- WebServer/main.cpp
+++ WebServer/main.cpp
@@ -12,6 +12,24 @@ using namespace std;
 int main(int argc, char *argv[]) {
     int threadNum = 5;
     int port = 80;
+
+    int opt;
+    const char *str = "t:l:p:";
+    while ((opt = getopt(argc, argv, str)) != -1) {
+        switch (opt) {
+            case 't': {
+                threadNum = atoi(optarg);
+                break;
+            }
+            case 'p': {
+                port = atoi(optarg);
+                break;
+            }
+            default:
+                break;
+        }
+    }
+
     EventLoop mainLoop;
     Server myHTTPServer(&mainLoop, threadNum, port);
     myHTTPServer.start();
